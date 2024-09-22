c918c2138a7d1ab4b1d9bef6b4e00a28fde517a6
qwertyAL <sergeyiksanov25@gmail.com>
2024-09-22 21:15:10 +0300

    change site 104

--- site_new/src/components/RatingScreen.js
+++ site_new/src/components/RatingScreen.js
@@ -9,17 +9,47 @@ const users = [
   //<User>Юзернейм: {user.username} | Своишки: {user.points} | Уровень: {user.level}</User>
   
 const RatingScreen = () => {
-  const items = users.map((user) => {
-    console.log(user)
-    return (
-      <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '16px', marginBottom: '16px'}}>
-        <User>Юзернейм: {user.username} | Своишки: {user.points} | Уровень: {user.level}</User>
-      </div>
-    );
+  const [levels, setLevels] = useState([]);
+  fetch(baseUrl + "/levels", {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'ngrok-skip-browser-warning': true
+    }
+  }).then(response => response.text())
+  .then(data => {
+    console.log(JSON.parse(data).data);
+    setLevels(JSON.parse(data).data);
+  })
+
+
+  const items = levels.map((level) => {
+    console.log(level)
+    if (level.ID == 1) {
+      return (
+        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '16px', marginBottom: '16px', border: '2px solid #33ff3c', padding: '16px', borderRadius: '16px'}}>
+          <img src={ButtonImage} width={'100px'} style={{marginRight: '16px'}}/>
+          <div>
+            <p style={{fontWeight: 'bold'}}>{level.Name}</p>
+            <p>Необходимо: {level.NeedPoints}</p>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '16px', marginBottom: '16px', padding: '16px', borderRadius: '16px', background: '#393439'}}>
+          <img src={LockImage} width={'100px'} style={{marginRight: '16px'}}/>
+          <div>
+            <p style={{fontWeight: 'bold'}}>{level.Name}</p>
+            <p>Необходимо: {level.NeedPoints}</p>
+          </div>
+        </div>
+      );
+    }
   });
   
   return (
-    <div className="rating-screen">
+    <div className="levels-screen">
       {items}
     </div>
   );
