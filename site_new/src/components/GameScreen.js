064d1860c4752c8f9a40fef4ce3fae8c6b8f0767
qwertyAL <sergeyiksanov25@gmail.com>
2024-09-22 09:39:29 +0300

    change site 53

--- site_new/src/components/GameScreen.js
+++ site_new/src/components/GameScreen.js
@@ -19,60 +19,8 @@ const GameScreen = () => {
   const [loading, setLoading] = useState(true); // Лоадер
 
   const navigate = useNavigate();
-  const intervalRef = useRef(null);
 
   useEffect(() => {
-    const sendPointsToServer = async () => {
-      if (pointsToSend > 0) {
-        try {
-          const response = await fetch(baseUrl + '/users/add_points', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'ngrok-skip-browser-warning': true
-            },
-            body: JSON.stringify({
-              userId: String(initDataUnsafe.id),
-              points: pointsToSend
-            })
-          });
-  
-          const userData = await response.json();
-          
-          // Обновляем состояние пользователя и уровень
-          setUser(userData.data);
-          setPoints(userData.data.CountPoints);
-  
-          // Если уровень изменился, отправляем новый запрос для получения уровня
-          if (userData.data.LevelID !== user.LevelID) {
-            const levelResponse = await fetch(baseUrl + "/level?id=" + String(userData.data.LevelID), {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-                'ngrok-skip-browser-warning': true
-              },
-            });
-            const levelData = await levelResponse.json();
-            setLevel(levelData.data);
-            const nextLevelResponse = await fetch(baseUrl + "/level?id=" + String(userData.data.LevelID + 1), {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-                'ngrok-skip-browser-warning': true
-              },
-            });
-            const nextLevelData = await nextLevelResponse.json();
-            if (nextLevelData.data.NeedPoints !== null) {
-              setNextLevel(nextLevelData.data);
-            }
-          }
-  
-          setPointsToSend(0); // Сбрасываем количество отправленных очков
-        } catch (error) {
-          console.error('Error sending points:', error);
-        }
-      }
-    };
 
     const fetchData = async () => {
       try {
@@ -119,22 +67,13 @@ const GameScreen = () => {
 
     fetchData();
 
-    intervalRef.current = setInterval(() => {
-      sendPointsToServer();
-    }, 10000);
+    const interval = setInterval(() => {
+      setPoints(points + 1);
+    }, 1000);
 
-    // Отправляем очки перед закрытием/обновлением страницы
-    const handleBeforeUnload = () => {
-      sendPointsToServer();
-    };
-    window.addEventListener('beforeunload', handleBeforeUnload);
-
-    return () => {
-      clearInterval(intervalRef.current); // Очищаем интервал при размонтировании
-      window.removeEventListener('beforeunload', handleBeforeUnload); // Удаляем обработчик события
-    };
-  }, [initDataUnsafe.id, initDataUnsafe.username]);
-
+    return () => clearInterval(interval);
+  })
+    
   if (loading) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
@@ -143,7 +82,7 @@ const GameScreen = () => {
     );
   }
 
-  const progress = points / nextLevel.NeedPoints;
+  const progress = points;
 
   return (
     <div className="game-screen" style={{ flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%', paddingTop: '100px' }}>
