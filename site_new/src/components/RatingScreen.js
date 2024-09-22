a297fb63826bb23e795c6f7bc63d8104d2df38f5
qwertyAL <sergeyiksanov25@gmail.com>
2024-09-22 21:28:43 +0300

    change site 107

--- site_new/src/components/RatingScreen.js
+++ site_new/src/components/RatingScreen.js
@@ -1,62 +1,36 @@
 import { User } from "@gravity-ui/uikit";
 import '@gravity-ui/uikit/styles/fonts.css';
 import '@gravity-ui/uikit/styles/styles.css';
-import ButtonImage from './../images/button-image.png';
-import LockImage from './../images/Lock@3x.svg';
-import { useState } from 'react';
-
-const users = [
-    { username: 'Player1', points: 150, level: '1' },
-    { username: 'Player2', points: 200, level: '2' },
-    // добавь остальных пользователей
-  ];
-
-  //<User>Юзернейм: {user.username} | Своишки: {user.points} | Уровень: {user.level}</User>
+import { useEffect, useState } from 'react';
   
 const baseUrl = '/api/api'
 
 const RatingScreen = () => {
-  const [levels, setLevels] = useState([]);
-  fetch(baseUrl + "/levels", {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'ngrok-skip-browser-warning': true
-    }
-  }).then(response => response.text())
-  .then(data => {
-    console.log(JSON.parse(data).data);
-    setLevels(JSON.parse(data).data);
-  })
+  const [rating, setRating] = useState([]);
 
+  useEffect(() => {
+    fetch(baseUrl + "/rating", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': true
+      }
+    }).then(response => response.text())
+    .then(data => {
+      console.log(JSON.parse(data).data);
+      setRating(JSON.parse(data).data);
+    })
+  });
 
-  const items = levels.map((level) => {
-    console.log(level)
-    if (level.ID == 1) {
-      return (
-        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '16px', marginBottom: '16px', border: '2px solid #33ff3c', padding: '16px', borderRadius: '16px'}}>
-          <img src={ButtonImage} width={'100px'} style={{marginRight: '16px'}}/>
-          <div>
-            <p style={{fontWeight: 'bold'}}>{level.Name}</p>
-            <p>Необходимо: {level.NeedPoints}</p>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: '16px', marginBottom: '16px', padding: '16px', borderRadius: '16px', background: '#393439'}}>
-          <img src={LockImage} width={'100px'} style={{marginRight: '16px'}}/>
-          <div>
-            <p style={{fontWeight: 'bold'}}>{level.Name}</p>
-            <p>Необходимо: {level.NeedPoints}</p>
-          </div>
-        </div>
-      );
-    }
+  const items = rating.map((user) => {
+    console.log(user)
+    return (
+      <User>Юзернейм: {user.Username} | Своишки: {user.Points} | Уровень: {user.LevelID}</User>
+    );
   });
   
   return (
-    <div className="levels-screen">
+    <div className="rating-screen">
       {items}
     </div>
   );
